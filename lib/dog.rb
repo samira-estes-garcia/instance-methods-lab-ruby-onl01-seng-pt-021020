a842445f1a35e5c5ccbaf7a7a0365a9706672197
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-01 02:51:52 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+# add class def for Dog 
+
+class Dog 
+  # def an instance method called #bark that puts "Woof!"
+  def bark
+    puts "Woof!"
+  end
+  # def #sit to Dog class, puts "The Dog is sitting"
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+
